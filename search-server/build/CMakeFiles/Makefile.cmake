408d801ed6f516c9cd4bf96359262216b17c2a9d
Nikolai <nikolay.gromov.u@yandex.ru>
2023-03-25 18:11:27 +0300

    sprint 5: final project

--- search-server/build/CMakeFiles/Makefile.cmake
+++ search-server/build/CMakeFiles/Makefile.cmake
@@ -20,23 +20,16 @@ set(CMAKE_MAKEFILE_DEPENDS
   "/usr/share/cmake-3.16/Modules/CMakeLanguageInformation.cmake"
   "/usr/share/cmake-3.16/Modules/CMakeSystemSpecificInformation.cmake"
   "/usr/share/cmake-3.16/Modules/CMakeSystemSpecificInitialize.cmake"
-  "/usr/share/cmake-3.16/Modules/CPack.cmake"
-  "/usr/share/cmake-3.16/Modules/CPackComponent.cmake"
-  "/usr/share/cmake-3.16/Modules/CTest.cmake"
-  "/usr/share/cmake-3.16/Modules/CTestTargets.cmake"
-  "/usr/share/cmake-3.16/Modules/CTestUseLaunchers.cmake"
   "/usr/share/cmake-3.16/Modules/Compiler/CMakeCommonCompilerMacros.cmake"
   "/usr/share/cmake-3.16/Modules/Compiler/GNU-C.cmake"
   "/usr/share/cmake-3.16/Modules/Compiler/GNU-CXX.cmake"
   "/usr/share/cmake-3.16/Modules/Compiler/GNU.cmake"
-  "/usr/share/cmake-3.16/Modules/DartConfiguration.tcl.in"
   "/usr/share/cmake-3.16/Modules/Internal/CMakeCheckCompilerFlag.cmake"
   "/usr/share/cmake-3.16/Modules/Platform/Linux-GNU-C.cmake"
   "/usr/share/cmake-3.16/Modules/Platform/Linux-GNU-CXX.cmake"
   "/usr/share/cmake-3.16/Modules/Platform/Linux-GNU.cmake"
   "/usr/share/cmake-3.16/Modules/Platform/Linux.cmake"
   "/usr/share/cmake-3.16/Modules/Platform/UnixPaths.cmake"
-  "/usr/share/cmake-3.16/Templates/CPackConfig.cmake.in"
   )
 
 # The corresponding makefile is:
@@ -47,41 +40,10 @@ set(CMAKE_MAKEFILE_OUTPUTS
 
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
-  "DartConfiguration.tcl"
-  "CPackConfig.cmake"
-  "CPackSourceConfig.cmake"
   "CMakeFiles/CMakeDirectoryInformation.cmake"
   )
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/NightlyMemCheck.dir/DependInfo.cmake"
-  "CMakeFiles/ContinuousConfigure.dir/DependInfo.cmake"
-  "CMakeFiles/ContinuousBuild.dir/DependInfo.cmake"
-  "CMakeFiles/NightlyConfigure.dir/DependInfo.cmake"
-  "CMakeFiles/NightlyMemoryCheck.dir/DependInfo.cmake"
-  "CMakeFiles/ExperimentalTest.dir/DependInfo.cmake"
-  "CMakeFiles/ContinuousUpdate.dir/DependInfo.cmake"
-  "CMakeFiles/NightlyUpdate.dir/DependInfo.cmake"
-  "CMakeFiles/ExperimentalMemCheck.dir/DependInfo.cmake"
-  "CMakeFiles/ExperimentalSubmit.dir/DependInfo.cmake"
-  "CMakeFiles/Experimental.dir/DependInfo.cmake"
-  "CMakeFiles/ContinuousCoverage.dir/DependInfo.cmake"
-  "CMakeFiles/ExperimentalConfigure.dir/DependInfo.cmake"
-  "CMakeFiles/Continuous.dir/DependInfo.cmake"
-  "CMakeFiles/NightlyTest.dir/DependInfo.cmake"
-  "CMakeFiles/main.dir/DependInfo.cmake"
-  "CMakeFiles/Nightly.dir/DependInfo.cmake"
-  "CMakeFiles/NightlySubmit.dir/DependInfo.cmake"
-  "CMakeFiles/NightlyStart.dir/DependInfo.cmake"
-  "CMakeFiles/ExperimentalCoverage.dir/DependInfo.cmake"
-  "CMakeFiles/NightlyCoverage.dir/DependInfo.cmake"
-  "CMakeFiles/ExperimentalStart.dir/DependInfo.cmake"
-  "CMakeFiles/NightlyBuild.dir/DependInfo.cmake"
-  "CMakeFiles/ExperimentalBuild.dir/DependInfo.cmake"
-  "CMakeFiles/ExperimentalUpdate.dir/DependInfo.cmake"
-  "CMakeFiles/ContinuousStart.dir/DependInfo.cmake"
-  "CMakeFiles/ContinuousMemCheck.dir/DependInfo.cmake"
-  "CMakeFiles/ContinuousTest.dir/DependInfo.cmake"
-  "CMakeFiles/ContinuousSubmit.dir/DependInfo.cmake"
+  "CMakeFiles/search_server.dir/DependInfo.cmake"
   )
